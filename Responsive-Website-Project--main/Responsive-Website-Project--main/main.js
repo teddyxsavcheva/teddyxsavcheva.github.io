c656564e246ef38e3a650dd934118735d560d5f2
Teodora Savcheva <115867929+teddyxsavcheva@users.noreply.github.com>
2024-03-26 17:13:35 +0200

    Delete Responsive-Website-Project--main/Responsive-Website-Project--main directory

--- Responsive-Website-Project--main/Responsive-Website-Project--main/main.js
+++
@@ -1,9 +0,0 @@
-// Shop now button to go to the products section 
-document.getElementById("shopNow").onclick = function () {
-        location.href = "#products";
-    };
-
-// Learn more button to go to the reviews section
-document.getElementById("learnMore").onclick = function () {
-        location.href = "#review";
-    };
